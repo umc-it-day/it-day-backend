d3634a02368de777ca17caf85869691e9317b191
dongkdc <dongkdc@naver.com>
2026-07-30 00:13:18 +0900

    refactor:혜택 엔티티 속성 수정 및 yml설정 수정

--- src/main/java/com/example/itday/domain/benefit/entity/Benefit.java
+++ src/main/java/com/example/itday/domain/benefit/entity/Benefit.java
@@ -37,9 +37,6 @@ public class Benefit {
     @Column(name = "description")
     private String description;
 
-    @Column(name = "benefitValue", nullable = false)
-    private Integer benefitValue;
-
     @Column(name = "validFrom", nullable = false)
     private LocalDate validFrom;
 
