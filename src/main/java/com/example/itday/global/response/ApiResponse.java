8ec248b23fdc832184b8f6c7d4e2614176bb8242
ParkSol <iamsorry7942@gmail.com>
2026-07-31 20:54:44 +0900

    Add files via upload

---
+++ src/main/java/com/example/itday/global/response/ApiResponse.java
@@ -0,0 +1,48 @@
+package com.example.itday.global.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ApiResponse<T> {
+
+    private boolean success;
+    private String message;
+    private T data;
+
+    public static <T> ApiResponse<T> success(T data) {
+        return new ApiResponse<>(
+                true,
+                "요청이 성공적으로 처리되었습니다.",
+                data
+        );
+    }
+
+    public static <T> ApiResponse<T> success(String message, T data) {
+        return new ApiResponse<>(
+                true,
+                message,
+                data
+        );
+    }
+
+    public static ApiResponse<Void> success(String message) {
+        return new ApiResponse<>(
+                true,
+                message,
+                null
+        );
+    }
+
+    public static ApiResponse<Void> failure(String message) {
+        return new ApiResponse<>(
+                false,
+                message,
+                null
+        );
+    }
+}
